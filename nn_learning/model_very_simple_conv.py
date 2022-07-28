2975dae7e9ba3f89b6ef0acc337fe4449695911e
huber2 <66030895+huber2@users.noreply.github.com>
2022-07-28 17:27:30 +0100

    more augmentation mask size changes

---
+++ nn_learning/model_very_simple_conv.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+class ConvNet(nn.Module):   
+    def __init__(self, n_classes):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=4, stride=1, padding=0),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(64, 16),
+            nn.ReLU(inplace=True),
+            nn.Linear(16, n_classes),
+        )  
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
